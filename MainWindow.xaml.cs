4558a3a7b3f30a5f17dfa02f98aa3b2a632a4122
tran0uz <tran0uz@gmail.com>
2024-11-17 02:12:44 +0100

    Button Add

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,5 +29,100 @@ namespace MagicCalculator
         {
             
         }
+
+        private void btn_equal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_plus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_sub_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_mul_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_div_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_del_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_comma_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_num_9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_num_6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_num_3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_num_0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_num_8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_num_5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_num_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_num_vorzeichen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_num_7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_num_4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_num_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
