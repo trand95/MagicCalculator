e9f24af984b1e4dcaf69a92815cfb1b811b214f7
trand95 <trandavid@hotmail.de>
2024-11-18 17:27:45 +0100

    Bug Fix History and txtCalulation.Text

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,7 +5,7 @@ namespace MagicCalculator
 {
     public partial class MainWindow : Window
     {
-        // Variable zur Speicherung des aktuellen Eingabewerts
+
         private string _currentInput = "";
         private string _operator = "";
         private string _history = "";
@@ -22,7 +22,7 @@ namespace MagicCalculator
             InitializeComponent();
         }
 
-        // Event-Handler für die Zahlentasten
+
         private void btn_num_1_Click(object sender, RoutedEventArgs e) => AddToInput("1");
         private void btn_num_2_Click(object sender, RoutedEventArgs e) => AddToInput("2");
         private void btn_num_3_Click(object sender, RoutedEventArgs e) => AddToInput("3");
@@ -33,18 +33,19 @@ namespace MagicCalculator
         private void btn_num_8_Click(object sender, RoutedEventArgs e) => AddToInput("8");
         private void btn_num_9_Click(object sender, RoutedEventArgs e) => AddToInput("9");
         private void btn_num_0_Click(object sender, RoutedEventArgs e) => AddToInput("0");
-
-        // Event-Handler für den Komma-Button
         private void btn_comma_Click(object sender, RoutedEventArgs e) => AddToInput(",");
+        private void btn_plus_Click(object sender, RoutedEventArgs e) => SetOperator("+");
+        private void btn_sub_Click(object sender, RoutedEventArgs e) => SetOperator("-");
+        private void btn_mul_Click(object sender, RoutedEventArgs e) => SetOperator("x");
+        private void btn_div_Click(object sender, RoutedEventArgs e) => SetOperator("/");
 
-        // Hinzufügen von Zeichen (Zahlen oder Operatoren) zum aktuellen Eingabewert
         private void AddToInput(string value)
         {
 
             if (_isEqual == true)
             {
                 txtCalculation.Text = "";
-                
+
                 _currentInput = "";
                 txtHistory.Text = "";
                 _isEqual = false;
@@ -64,25 +65,8 @@ namespace MagicCalculator
                 _currentInput += value;
             }
 
-
             txtCalculation.Text = _currentInput;
-
-
-
-
-
-
-
-
-
         }
-
-        // Event-Handler für die Operatoren
-        private void btn_plus_Click(object sender, RoutedEventArgs e) => SetOperator("+");
-        private void btn_sub_Click(object sender, RoutedEventArgs e) => SetOperator("-");
-        private void btn_mul_Click(object sender, RoutedEventArgs e) => SetOperator("x");
-        private void btn_div_Click(object sender, RoutedEventArgs e) => SetOperator("/");
-
         private void ReplaceLastChar(string input, string newOperator)
         {
             if (string.IsNullOrEmpty(input))
@@ -95,295 +79,73 @@ namespace MagicCalculator
             txtHistory.Text = input.Substring(0, input.Length - 1) + newOperator;
             return;
         }
-        // Setzen des Operators und Speichern des ersten Operanden
+
         private void SetOperator(string op)
         {
+            if (double.TryParse(_currentInput, out double parsedValue))
+            {
+                if (_operand1 == 0.0 && _operator == "")
+                {
+
+                    _operand1 = parsedValue;
+                }
+                else
+                {
+
+                    _operand2 = parsedValue;
+                    PerformCalculation();
+                    _operand1 = _result;
+                }
+            }
 
 
             _operator = op;
+            _currentInput = "";
             _operatorCounter++;
 
 
+            txtHistory.Text = $"{_operand1} {_operator}";
+            txtCalculation.Text = _operand1.ToString();
+        }
 
-
+        private void PerformCalculation()
+        {
             switch (_operator)
             {
                 case "+":
-
-                    if (_operatorCounter > 1)
-                    {
-                        ReplaceLastChar(txtHistory.Text, _operator);
-
-                    }
-                    else
-                    {
-                        txtHistory.Text += " " + txtCalculation.Text + " +";
-                    }
+                    _result = _operand1 + _operand2;
                     break;
                 case "-":
-
-                    if (_operatorCounter > 1)
-                    {
-                        ReplaceLastChar(txtHistory.Text, _operator);
-
-                    }
-                    else
-                    {
-                        txtHistory.Text += " " + txtCalculation.Text + " -";
-                    }
-                    break;
-                case "/":
-
-                    if (_operatorCounter > 1)
-                    {
-                        ReplaceLastChar(txtHistory.Text, _operator);
-
-                    }
-                    else
-                    {
-                        txtHistory.Text += " " + txtCalculation.Text + " /";
-                    }
+                    _result = _operand1 - _operand2;
                     break;
                 case "x":
-
-                    if (_operatorCounter > 1)
+                    _result = _operand1 * _operand2;
+                    break;
+                case "/":
+                    if (_operand2 == 0)
                     {
-                        ReplaceLastChar(txtHistory.Text, _operator);
-
+                        txtCalculation.Text = "Fehler: Division durch 0";
+                        ResetCalculator();
+                        return;
                     }
-                    else
-                    {
-                        txtHistory.Text += " " + txtCalculation.Text + " x";
-                    }
+                    _result = _operand1 / _operand2;
                     break;
             }
 
-            if (_operand1 == 0.0)
-            {
-                if (double.TryParse(_currentInput, out double parsedValue))
-                {
-                    _operand1 = parsedValue;
-                }
-                _currentInput = "";
-            }
-
-            else if (_operand2 == 0.0)
-            {
-                if (double.TryParse(_currentInput, out double parsedValue))
-                {
-                    _operand2 = parsedValue;
-                }
-                _currentInput = "";
-
-                switch (_operator)
-                {
-                    case "+":
-
-                        if (_operatorCounter > 1)
-                        {
-                            ReplaceLastChar(txtHistory.Text, _operator);
-
-                        }
-                        else
-                        {
-                            // txtHistory.Text += " " + txtCalculation.Text + " +";
-                        }
-                        _result = _operand1 + _operand2;
-
-                        txtCalculation.Text = _result.ToString();
-                        _operand1 = _result;
-                        _operand2 = 0.0;
-                        break;
-                    case "-":
-
-                        if (_operatorCounter > 1)
-                        {
-                            ReplaceLastChar(txtHistory.Text, _operator);
-
-                        }
-                        else
-                        {
-                            //txtHistory.Text += " " + txtCalculation.Text + " -";
-                        }
-                        _result = _operand1 - _operand2;
-
-                        txtCalculation.Text = _result.ToString();
-                        _operand1 = _result;
-                        _operand2 = 0.0;
-                        break;
-                    case "/":
-
-                        if (_operatorCounter > 1)
-                        {
-                            ReplaceLastChar(txtHistory.Text, _operator);
-
-                        }
-                        else
-                        {
-                            // txtHistory.Text += " " + txtCalculation.Text + " /";
-                        }
-                        _result = _operand1 / _operand2;
-
-                        txtCalculation.Text = _result.ToString();
-                        _operand1 = _result;
-                        _operand2 = 0.0;
-                        break;
-                    case "x":
-
-                        if (_operatorCounter > 1)
-                        {
-                            ReplaceLastChar(txtHistory.Text, _operator);
-
-                        }
-                        else
-                        {
-                            //txtHistory.Text += " " + txtCalculation.Text + " x";
-                        }
-                        _result = _operand1 * _operand2;
-
-                        txtCalculation.Text = _result.ToString();
-                        _operand1 = _result;
-                        _operand2 = 0.0;
-                        break;
-                }
-            }
-
-
-            /*
-
-            _operator = op;
-            
-            
-            if (_operator != op)
-            {
-                _operator = op;
-                _operatorIsChanged = true;
-            }
-
-
-            if (_operand1 == 0.0)
-            {
-                _operand1 = double.Parse(_currentInput);
-
-                switch (_operator)
-                {
-                    case "+":
-                        txtHistory.Text += " " + txtCalculation.Text + " +";
-                        if (_operatorIsChanged == true)
-                        {
-                            ReplaceLastChar(txtHistory.Text, _operator);
-                            _operatorIsChanged = false;
-                        }
-                        break;
-                    case "-":
-                        txtHistory.Text += " " + txtCalculation.Text + " -";
-                        if (_operatorIsChanged == true)
-                        {
-                            ReplaceLastChar(txtHistory.Text, _operator);
-                            _operatorIsChanged = false;
-                        }
-                        break;
-                    case "/":
-                        txtHistory.Text += " " + txtCalculation.Text + " /";
-                        if (_operatorIsChanged == true)
-                        {
-                            ReplaceLastChar(txtHistory.Text, _operator);
-                            _operatorIsChanged = false;
-                        }
-                        break;
-                    case "*":
-                        txtHistory.Text += " " + txtCalculation.Text + " *";
-                        if (_operatorIsChanged == true)
-                        {
-                            ReplaceLastChar(txtHistory.Text, _operator);
-                            _operatorIsChanged = false;
-                        }
-                        break;
-                }
-            }
-
-
-
-            else if (_operand2 == 0.0)
-            {
-                if (_currentInput != "")
-                {
-                    _operand2 = double.Parse(_currentInput);
-                    _currentInput = "";
-
-                    switch (_operator)
-                    {
-                        case "+":
-                            _result = _operand1 + _operand2;
-                            txtHistory.Text += " " + txtCalculation.Text + " +";
-                            txtCalculation.Text = _result.ToString();
-                            _operand1 = _result;
-                            _operand2 = 0.0;
-                            if (_operatorIsChanged == true)
-                            {
-                                ReplaceLastChar(txtHistory.Text, _operator);
-                                _operatorIsChanged = false;
-                            }
-                            break;
-                        case "-":
-                            _result = _operand1 - _operand2;
-                            txtHistory.Text += " " + txtCalculation.Text + " -";
-                            txtCalculation.Text = _result.ToString();
-                            _operand1 = _result;
-                            _operand2 = 0.0;
-                            if (_operatorIsChanged == true)
-                            {
-                                ReplaceLastChar(txtHistory.Text, _operator);
-                                _operatorIsChanged = false;
-                            }
-                            break;
-                        case "/":
-                            _result = _operand1 / _operand2;
-                            txtHistory.Text += " " + txtCalculation.Text + " /";
-                            txtCalculation.Text = _result.ToString();
-                            _operand1 = _result;
-                            _operand2 = 0.0;
-                            if (_operatorIsChanged == true)
-                            {
-                                ReplaceLastChar(txtHistory.Text, _operator);
-                                _operatorIsChanged = false;
-                            }
-                            break;
-                        case "*":
-                            _result = _operand1 * _operand2;
-                            txtHistory.Text += " " + txtCalculation.Text + " *";
-                            txtCalculation.Text = _result.ToString();
-                            _operand1 = _result;
-                            _operand2 = 0.0;
-                            if (_operatorIsChanged == true)
-                            {
-                                ReplaceLastChar(txtHistory.Text, _operator);
-                                _operatorIsChanged = false;
-                            }
-                            break;
-                    }
-                }
-
-
-
-
-
-            }
-
-
-
-
-
-
-            */
-
-
-
+            _operand1 = _result;
+            _operand2 = 0.0;
         }
 
-
-
-        // Event-Handler für den Gleichheits-Button
+        private void ResetCalculator()
+        {
+            _operator = "";
+            _currentInput = "";
+            _operand1 = 0.0;
+            _operand2 = 0.0;
+            _result = 0.0;
+            txtCalculation.Text = "";
+            txtHistory.Text = "";
+        }
         private void btn_equal_Click(object sender, RoutedEventArgs e)
         {
 
@@ -437,8 +199,6 @@ namespace MagicCalculator
             _isEqual = true;
 
         }
-
-        // Event-Handler für den DEL-Button (Löschen)
         private void btn_del_Click(object sender, RoutedEventArgs e)
         {
             txtCalculation.Text = "";
@@ -448,8 +208,6 @@ namespace MagicCalculator
 
 
         }
-
-        // Event-Handler für den Vorzeichen-Button (+/-)
         private void btn_vorzeichen_Click(object sender, RoutedEventArgs e)
         {
             if (double.TryParse(_currentInput, out double parsedValue))
@@ -459,11 +217,9 @@ namespace MagicCalculator
                 txtCalculation.Text = _currentInput;
             }
         }
-
-        // Event-Handler für den TextChanged-Event der TextBox (optional)
         private void txtCalculation_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
-            // Optional: Weitere Validierungen oder Updates der UI
+            
         }
     }
 }
